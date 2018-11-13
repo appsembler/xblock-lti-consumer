7ca220f1c0d074ee4e8842a60076c5f8747f53e3
Omar Al-Ithawi <i@omardo.com>
2018-11-13 13:41:40 +0200

    Revert "return Microsoft as course org, for MSFT courses"
    This reverts commit 4fd06da49e634ca6e2f13b8d1e70b565657cd728.

--- lti_consumer/lti_consumer.py
+++ lti_consumer/lti_consumer.py
@@ -452,10 +452,7 @@ class LtiConsumerXBlock(StudioEditableXBlockMixin, XBlock):
         context_id is an opaque identifier that uniquely identifies the context (e.g., a course)
         that contains the link being launched.
         """
-        if self.course.is_microsoft_course:
-            return unicode(re.sub('\:.*?\+', ':Microsoft+', self.course_id))
-        else:
-            return unicode(self.course_id)  # pylint: disable=no-member
+        return unicode(self.course_id)  # pylint: disable=no-member
 
     @property
     def role(self):
